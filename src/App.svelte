810a2e8a49ad1dee2bf6c41a32714c7786f7d15d
sabbirz <sabbir.kuasha@gmail.com>
2022-07-14 14:08:45 +0600

    Form svelte - 1
    2 way binding of form

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,21 @@
 <script>
- 
+    let inputValue = 'Default Text'
+
+
+    function setValue(event){
+        inputValue = event.target.value
+    }
 </script>
 
 <main>
-   
+   <h1>Binding form </h1>
+
+   <h2 class="p-5"
+   >Input Value is: {inputValue}</h2>
+   <input class="input input-bordered 
+   input-info w-full max-w-xs ml-5"
+   type="text" placeholder="Type Here"
+   value={inputValue} on:input={setValue}>
 </main>
 
 <style>
