d5188c1516bdecdf0952599024079e4e7235dca6
sabbirz <sabbir.kuasha@gmail.com>
2022-07-14 12:10:02 +0600

    learning slot and props

---
+++ src/Modal.svelte
@@ -0,0 +1,34 @@
+<script>
+    
+</script>
+
+
+<label  for="my-modal" 
+        class="btn modal-button">
+    open modal
+</label>
+
+
+<input  type="checkbox" 
+        id="my-modal" 
+        class="modal-toggle" 
+/>
+<div class="modal">
+    <div class="modal-box">
+        <h3 class="font-bold text-lg border-b-2">
+            <slot   name="Headline" 
+                    class="border"/>
+        </h3>
+        <p class="py-4">
+            <slot/>
+        </p>
+        <div class="modal-action">
+            <slot name="footer">
+                <label for="my-modal" class="btn" disabled>
+                    Agree with the terms & coonditions
+                </label>
+            </slot>
+            
+        </div>
+    </div>
+</div>
---
+++ src/Products.svelte
@@ -0,0 +1,62 @@
+<script>
+    import {createEventDispatcher} from 'svelte'
+
+    let dispatch = createEventDispatcher()
+
+    export let price = '1200'
+    export let ProductName
+    export let id = '0'
+    export let badge = null
+
+    
+
+function addtocart(){
+    dispatch('add-to-cart-btn', 
+        {
+            id : id,
+            ProductName: ProductName,
+            price: price,
+            badge: badge
+        })
+}
+function deletebtn(){
+    dispatch('delete-btn',
+        {
+            id : id,
+            ProductName: ProductName,
+            price: price,
+            badge: badge
+        })
+}
+</script>
+
+<main>
+    <article>
+        <div class="p-6">
+            {#if badge}
+                <div class="badge badge-secondary badge-outline">
+                    {badge}
+                </div>    
+            {/if}
+            
+
+            <h1 class="pb-2 text-3xl">
+                {id}: {ProductName}
+            </h1>
+            
+            <h2 class="pb-2 text-2xl">
+                Price: {price}
+            </h2>
+            
+            <button on:click = {addtocart}
+            class="mr-3 btn btn-primary">
+                Add to cart
+            </button>
+            <button on:click = {deletebtn}
+            class="btn btn-error">
+                Delete
+            </button>
+        </div>
+        
+    </article>
+</main>
