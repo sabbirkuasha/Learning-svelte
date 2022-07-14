0656aaa46516b92ca82b0779ed4301d363f0ba6c
sabbirz <sabbir.kuasha@gmail.com>
2022-07-14 14:18:09 +0600

    Bind input in svelte
    2nd approach to bind input value

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script>
     let inputValue = 'Default Text'
+    let inputValue2 = 'Default text2'
 
+    //this means everytime inputValue varibale changes, console log the inputValue
+    $:console.log(inputValue)
 
     function setValue(event){
         inputValue = event.target.value
@@ -8,14 +11,23 @@
 </script>
 
 <main>
-   <h1>Binding form </h1>
-
+   <h1 class="text-2xl font-bold p-5">Binding form </h1>
+   
    <h2 class="p-5"
    >Input Value is: {inputValue}</h2>
    <input class="input input-bordered 
    input-info w-full max-w-xs ml-5"
    type="text" placeholder="Type Here"
    value={inputValue} on:input={setValue}>
+   
+<!-- This is shorthand in svelte -->
+   <h1 class="text-2xl font-bold p-5">Binding form Approach 2</h1>
+   <h2 class="p-5"
+   >Input Value is: {inputValue2}</h2>
+   <input class="input input-bordered 
+   input-info w-full max-w-xs ml-5"
+   type="text" placeholder="Type Here"
+   bind:value={inputValue2}>
 </main>
 
 <style>
